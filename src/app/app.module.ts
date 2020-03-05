565dc1a2150d805a07cac9b7373860ac50cc9899
Dmitriy <plekhanov84@gmail.com>
2020-03-05 23:22:37 +0200

    add storage service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+import { StorageService } from './services/storage.service';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     AngularFireAuthModule,
     NgbModule
   ],
-  providers: [],
+  providers: [StorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
